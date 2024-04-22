3d080ee4951bc2ad214a213e4aab9a6dbbf9acc4
unknown <fauziyahdewi21@gmail.com>
2024-04-22 17:06:50 +0700

    service, about, contact

--- resources/js/Pages/Home/Partials/AboutSection.jsx
+++ resources/js/Pages/Home/Partials/AboutSection.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Container from '@/Components/Home/Container'
+import SectionTitle from '@/Components/Home/SectionTitle'
+import { Text, Image } from '@mantine/core';
 
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
+    <div>
+      <Container>
+        <div>
+          <SectionTitle>Tentang Kami</SectionTitle>
+        </div>
+        <div className='mt-5'>
+          <Image
+            radius="md"
+            h={200}
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png"
+          />
+          <div className='text-center mt-5'>
+            <Text className=''>
+              Sambutlah pengalaman berbelanja kaos jersey yang tak tertandingi di MyJersey, destinasi utama untuk semua kebutuhan jersey olahraga Anda. Kami menawarkan beragam pilihan kaos jersey berkualitas tinggi untuk memenuhi kebutuhan penggemar olahraga sejati. Di MyJersey, kami mengerti betapa pentingnya setiap detail dalam menciptakan pengalaman yang memuaskan bagi penggemar olahraga. Dari kaos jersey tim favorit Anda hingga koleksi unik yang sulit ditemukan di tempat lain, kami menawarkan berbagai macam desain, ukuran, dan varian untuk memenuhi selera dan kebutuhan Anda.Kami berkomitmen untuk memberikan pelayanan terbaik kepada pelanggan kami. Dengan layanan pelanggan yang responsif dan pengalaman berbelanja yang mulus, MyJersey memberikan kemudahan dan kepuasan maksimal kepada setiap pelanggan. Temukan kaos jersey yang sesuai dengan gaya dan preferensi Anda hanya di MyJersey. Dapatkan koleksi terbaik dan jadilah bagian dari penggemar olahraga yang bergaya dan bersemangat.
+            </Text>
+          </div>
+        </div>
+
+      </Container>
+    </div>
   )
 }
 
---
+++ resources/js/Pages/Home/Partials/ContactSection/ContactSection.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Container from '@/Components/Home/Container'
+import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid } from '@mantine/core';
+import { ContactIconsList } from './ContactIcon';
+import classes from '../../css/ContactInfo.module.css';
+
+const ContactSection = () => {
+  return (
+    <div>
+      <Container>
+        <div className='mt-5 mx-20'>
+          <Paper shadow="md" radius="lg">
+            <div className={classes.wrapper}>
+              <div className={classes.contacts}>
+                <Text fz="lg" fw={700} className={classes.title} c="#fff">
+                  Contact information
+                </Text>
+
+                <ContactIconsList />
+              </div>
+
+              <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+                <Text fz="lg" fw={700} className={classes.title}>
+                  Get in touch
+                </Text>
+
+                <div className={classes.fields}>
+                  <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                    <TextInput label="Your name" placeholder="Your name" />
+                    <TextInput label="Your email" placeholder="youremail@gmail.com" required />
+                  </SimpleGrid>
+
+                  <TextInput mt="md" label="Subject" placeholder="Subject" required />
+
+                  <Textarea
+                    mt="md"
+                    label="Your message"
+                    placeholder="Please include all relevant information"
+                    minRows={3}
+                  />
+
+                  <Group justify="flex-end" mt="md">
+                    <Button type="submit" className={classes.control}>
+                      Send message
+                    </Button>
+                  </Group>
+                </div>
+              </form>
+            </div>
+          </Paper>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default ContactSection
